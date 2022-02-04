db6d600417d17de3258785af9810741e6f800bbb
Drobenyuk Andriy <adrobenyuk@shopmonkey.io>
2022-02-04 16:45:46 +0200

    create base forms for comparison

---
+++ src/components/PromoCodeForm/PromoCodeForm.tsx
@@ -0,0 +1,14 @@
+const PromoCodeForm = () => {
+  return (
+    <form className="card p-2">
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Promo code" />
+        <button type="submit" className="btn btn-secondary">
+          Redeem
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default PromoCodeForm;
