6fb9181f7fce8483ea47f491a69e73c8113120be
Drobenyuk Andriy <adrobenyuk@shopmonkey.io>
2022-02-09 18:12:44 +0200

    Wrap with react hooks form and yup validation

--- src/components/BillingForm/Form.tsx
+++ src/components/BillingForm/Form.tsx
@@ -1,68 +1,118 @@
+import React from "react";
+import { useForm } from "react-hook-form";
 import { Checkbox, Radio, TextField } from "@shopmonkeyus/ui-kit";
 
+import validation from "./validation";
+import { getValidationStatus } from "./";
+
 const BillingForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: validation,
+  });
+
+  const onSubmit = <T extends unknown>(data: T) => console.log(data);
+
+  console.log({
+    BillingForm: "rendered",
+  });
+
+  const sameAddressField = register("isSameAddress");
+  const saveInfoField = register("canSaveInfo");
+  const paymentField = register("payment", { required: true });
+
+  const handleSameAddressChange = React.useCallback(
+    (value: boolean, e: React.ChangeEvent<HTMLInputElement>) => {
+      sameAddressField.onChange(e);
+    },
+    [sameAddressField]
+  );
+
+  const handleSaveInfoChange = React.useCallback(
+    (value: boolean, e: React.ChangeEvent<HTMLInputElement>) => {
+      saveInfoField.onChange(e);
+    },
+    [saveInfoField]
+  );
+
+  const handlePaymentChange = React.useCallback(
+    (value: string, e: React.ChangeEvent<HTMLInputElement>) => {
+      paymentField.onChange(e);
+    },
+    [paymentField]
+  );
+
   return (
-    <form className="needs-validation">
+    <form className="needs-validation" onSubmit={handleSubmit(onSubmit)}>
       <div className="row g-3">
         <div className="col-sm-6">
           <TextField
-            name="firstName"
+            {...register("firstName", { required: true })}
             label="First name"
             required={true}
-            errorMessage="Valid first name is required."
+            validationStatus={getValidationStatus(errors, "firstName")}
+            errorMessage={errors.firstName?.message}
           />
         </div>
 
         <div className="col-sm-6">
           <TextField
-            name="lastName"
+            {...register("lastName", { required: true })}
             label="Last name"
             required={true}
-            errorMessage="Valid last name is required."
+            validationStatus={getValidationStatus(errors, "lastName")}
+            errorMessage={errors.lastName?.message}
           />
         </div>
 
         <div className="col-12">
           <TextField
-            name="username"
-            label="Username"
+            {...register("username", { required: true })}
             required={true}
+            label="Username"
+            validationStatus={getValidationStatus(errors, "username")}
             iconBefore={() => (
               <span className="text-brand" style={{ lineHeight: 1 }}>
                 @
               </span>
             )}
-            errorMessage="Your username is required."
+            errorMessage={errors.username?.message}
           />
         </div>
 
         <div className="col-12">
           <TextField
-            name="email"
-            label="Email"
+            {...register("email", { required: true })}
             type="email"
+            label="Email"
+            required={true}
             placeholder="you@example.com"
-            errorMessage="Please enter a valid email address for shipping updates."
+            validationStatus={getValidationStatus(errors, "email")}
+            errorMessage={errors.email?.message}
           />
         </div>
 
         <div className="col-12">
           <TextField
-            name="address"
+            {...register("address", { required: true })}
+            required={true}
             label="Address"
-            required={true}
             placeholder="1234 Main St"
-            errorMessage="Please enter your shipping address."
+            validationStatus={getValidationStatus(errors, "address")}
+            errorMessage={errors.address?.message}
           />
         </div>
 
         <div className="col-12">
           <TextField
-            name="address2"
+            {...register("address2")}
             label="Address 2"
-            required={true}
             placeholder="Apartment or suite"
-            errorMessage="Please enter your shipping address."
+            validationStatus={getValidationStatus(errors, "address2")}
+            errorMessage={errors.address2?.message}
           />
         </div>
 
@@ -70,9 +120,9 @@ const BillingForm = () => {
           <label htmlFor="country" className="form-label">
             Country
           </label>
-          <select className="form-select" id="country" required={true}>
+          <select className="form-select" id="country" {...register("country")}>
             <option value="">Choose...</option>
-            <option>United States</option>
+            <option value="USA">United States</option>
           </select>
           <div className="invalid-feedback">Please select a valid country.</div>
         </div>
@@ -81,29 +131,32 @@ const BillingForm = () => {
           <label htmlFor="state" className="form-label">
             State
           </label>
-          <select className="form-select" id="state" required={true}>
+          <select className="form-select" id="state" {...register("state")}>
             <option value="">Choose...</option>
-            <option>California</option>
+            <option value="California">California</option>
           </select>
           <div className="invalid-feedback">Please provide a valid state.</div>
         </div>
 
         <div className="col-md-3">
           <TextField
-            name="zip"
+            {...register("zip", { required: true })}
             label="Zip"
             required={true}
-            errorMessage="Zip code required."
+            validationStatus={getValidationStatus(errors, "zip")}
+            errorMessage={errors.zip?.message}
           />
         </div>
       </div>
 
       <hr className="my-4" />
 
-      <Checkbox name="same-address">
+      <Checkbox {...sameAddressField} onChange={handleSameAddressChange}>
         Shipping address is the same as my billing address
       </Checkbox>
-      <Checkbox name="save-info">Save this information for next time</Checkbox>
+      <Checkbox {...saveInfoField} onChange={handleSaveInfoChange}>
+        Save this information for next time
+      </Checkbox>
 
       <hr className="my-4" />
 
@@ -111,13 +164,21 @@ const BillingForm = () => {
 
       <div className="my-3">
         <Radio.Group>
-          <Radio name="payment" value="credit">
+          <Radio
+            {...paymentField}
+            value="credit"
+            onChange={handlePaymentChange}
+          >
             Credit card
           </Radio>
-          <Radio name="payment" value="debit">
+          <Radio {...paymentField} value="debit" onChange={handlePaymentChange}>
             Debit card
           </Radio>
-          <Radio name="payment" value="paypal">
+          <Radio
+            {...paymentField}
+            value="paypal"
+            onChange={handlePaymentChange}
+          >
             PayPal
           </Radio>
         </Radio.Group>
@@ -126,38 +187,42 @@ const BillingForm = () => {
       <div className="row gy-3">
         <div className="col-md-6">
           <TextField
-            name="ccName"
+            {...register("ccName", { required: true })}
             label="Name on card"
             required={true}
-            errorMessage="Name on card is required"
+            validationStatus={getValidationStatus(errors, "ccName")}
+            errorMessage={errors.ccName?.message}
           />
           <small className="text-muted">Full name as displayed on card</small>
         </div>
 
         <div className="col-md-6">
           <TextField
-            name="ccNumber"
-            label="Credit card number"
+            {...register("ccNumber", { required: true, valueAsNumber: true })}
             required={true}
-            errorMessage="Credit card number is required"
+            label="Credit card number"
+            validationStatus={getValidationStatus(errors, "ccNumber")}
+            errorMessage={errors.ccNumber?.message}
           />
         </div>
 
         <div className="col-md-3">
           <TextField
-            name="ccExpiration"
+            {...register("ccExpiration", { required: true })}
             label="Expiration"
             required={true}
-            errorMessage="Expiration date required"
+            validationStatus={getValidationStatus(errors, "ccExpiration")}
+            errorMessage={errors.ccExpiration?.message}
           />
         </div>
 
         <div className="col-md-3">
           <TextField
-            name="ccCvv"
+            {...register("ccCvv", { required: true, valueAsNumber: true })}
             label="CVV"
             required={true}
-            errorMessage="Security code required"
+            validationStatus={getValidationStatus(errors, "ccCvv")}
+            errorMessage={errors.ccCvv?.message}
           />
         </div>
       </div>
